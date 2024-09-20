cbf4c62df424900321bb6a3c3fbbb35219486c9b
goodfish <songbc7@gmail.com>
2024-09-20 17:34:43 +0900

    프로젝트 시작

---
+++ js/main.js
@@ -0,0 +1,85 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  // logic..
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // badgeEl.style.display ='none';
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    } );
+  } else {
+    // badgeEl.style.display = 'block';
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300))
+
+
+
+const fadeELs = document.querySelectorAll('.visual .fade-in');
+fadeELs.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1
+  });
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+} );
+
+new Swiper('.promotion .swiper', {
+  slidesPerView: 3,   // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10,    // 슬라이드 사이 여백  px 기준
+  centeredSlides: true,   // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  //autoplay: {
+//    delay: 3000
+//  }   /* { 추가적인 옵션 할 수 있다. } */
+  pagination: {
+    el: '.promotion .swiper-pagination',  //페이지 번호 요소 선택자
+    clickable: true  // 사용자의 페이지 번호 요소 제어 가능
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    // 숨김처리
+    promotionEl.classList.add('hide');
+  } else {
+    promotionEl.classList.remove('hide');
+  }
+});
